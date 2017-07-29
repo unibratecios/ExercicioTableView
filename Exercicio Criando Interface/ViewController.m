95c1edf38f3e21279ca95c1a58b4befd232fbed1
Treinamento <treinamento@treinamento1-maquina2-2.local>
2017-07-29 14:30:53 -0300

    Initial Commit

---
+++ Exercicio Criando Interface/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Exercicio Criando Interface
+//
+//  Created by Treinamento on 29/07/17.
+//  Copyright © 2017 Unibratec. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Exercicio Criando Interface/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Exercicio Criando Interface
+//
+//  Created by Treinamento on 29/07/17.
+//  Copyright © 2017 Unibratec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
